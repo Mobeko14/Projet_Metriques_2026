de930b739ed554ff13edc439d47eb96780abbc2e
Edouard Junior Mobeko <mobekojunior@gmail.com>
2026-04-02 15:56:35 +0000

    Ajout graphique

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,23 @@ def home():
 @app.route('/histogramme')
 def histogramme():
     return render_template('histogramme.html')
+
+@app.route('/graphique')
+def graphique():
+    return render_template('graphique.html')
+
+@app.route('/paris')
+def paris():
+    data = [
+        {"datetime": "2026-04-01T00:00", "temperature_c": 15},
+        {"datetime": "2026-04-02T00:00", "temperature_c": 17},
+        {"datetime": "2026-04-03T00:00", "temperature_c": 16},
+        {"datetime": "2026-04-04T00:00", "temperature_c": 18},
+        {"datetime": "2026-04-05T00:00", "temperature_c": 20},
+        {"datetime": "2026-04-06T00:00", "temperature_c": 19},
+        {"datetime": "2026-04-07T00:00", "temperature_c": 21}
+    ]
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
